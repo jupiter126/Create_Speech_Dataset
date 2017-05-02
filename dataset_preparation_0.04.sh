0b81eab4fe16ba7e112fe038c456201a70332193
jupiter126 <jupiter126@gmail.com>
2017-05-02 02:46:32 +0200

    Update dataset_preparation_0.04.sh

--- dataset_preparation_0.04.sh
+++ dataset_preparation_0.04.sh
@@ -7,17 +7,6 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #############################################################
 
-#Purpose:
-# This scripts fetches open datasets of speech and transcription and aggregates them into a large metaset hopefully suitable for machine learning.
-
-#Requirements:
-# ffmpeg
-# parallel (O. Tange (2011): GNU Parallel - The Command-Line Power Tool ;login: The USENIX Magazine, February 2011:42-47.)
-
-#Special notes:
-# 1. I've used parallel (and sem) to spawn nproc $(nproc) ffmpeg (the number of cores in the machine).  Machine might be less responsive, and this leads to second note.... the more cores you have, the more it is important that you mount dataset on a separate drive.
-# 2. I mount a partition from another physical hard drive as the dataset folder, and recommend you do the same, in order to avoid having plenty of simultaneous read/write on the same drive.  On the one hand, this makes things much faster, on the other hand if you don't, this is really bad for your drive.
-
 #Versions:
 # 0.01 concept test
 # 0.02 serialised librispeech - used parallel
